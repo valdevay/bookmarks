61990fc68a61b26d04046b57111b1fae03794fcf
valdevay <valdevay@gmail.com>
2025-06-28 16:04:23 +0300

    first work version

--- main.go
+++ main.go
@@ -3,44 +3,65 @@ package main
 import "fmt"
 
 func main() {
+	fmt.Println("*** Bookmarks App ***")
+	bookmarks := map[string]string{}
 
-	actions := map[int]string{
-		1: "Посмотреть закладки",
-		2: "Добавить закладку",
-		3: "Удалить закладку",
-		4: "Выход",
+	for {
+		userAction := getMenu()
+
+		switch userAction {
+		case 1:
+			showBookmarks(bookmarks)
+		case 2:
+			bookmarks = addBookmark(bookmarks)
+		case 3:
+			bookmarks = removeBookmark(bookmarks)
+		case 4:
+			fmt.Println("Application closed")
+			break
+		}
 	}
+}
 
-	bookmarks := map[string]string{
-		"yandex": "https://yandex.ru/",
-		"google": "https://google.com/",
-	}
+func getMenu() int {
+	var userAction int
 
-	for keyMenu, valueMenu := range actions {
-		fmt.Println(keyMenu, valueMenu)
-	}
-
-	var userAction int8
-	var userBookmarkName string
-	var userBookmarkAdress string
-
+	fmt.Println("Choose the menu option")
+	fmt.Println("1. Show all bookmarks")
+	fmt.Println("2. Add a bookmark")
+	fmt.Println("3. Remove a bookmark")
+	fmt.Println("4. Exit the application")
 	fmt.Scan(&userAction)
+	return userAction
+}
 
-	switch actions {
-	case 1:
-		fmt.Println(bookmarks)
-	case 2:
-		fmt.Println("Please input bookmark name")
-		fmt.Scan(&userBookmarkName)
-		fmt.Println("Please input bookmark adress")
-		fmt.Scan(&userBookmarkName)
-		bookmarks[userBookmarkName] = userBookmarkAdress
-	case 3:
-		fmt.Println("Please input bookmark name")
-		fmt.Scan(&userBookmarkName)
-		delete(bookmarks, userBookmarkName)
-		fmt.Println(userBookmarkName, "has been deleted")
-		fmt.Println("Bookmark has", bookmarks)
-
+func showBookmarks(bookmarks map[string]string) {
+	if len(bookmarks) == 0 {
+		fmt.Println("You have no bookmarks yet")
 	}
+	for key, value := range bookmarks {
+		fmt.Println(key, ": ", value)
+	}
+}
+
+func addBookmark(bookmarks map[string]string) map[string]string {
+	var newNameBookmark string
+	var newAdressBookmark string
+
+	fmt.Println("Please input bookmark name")
+	fmt.Scan(&newNameBookmark)
+	fmt.Println("Please input bookmark adress")
+	fmt.Scan(&newAdressBookmark)
+	bookmarks[newNameBookmark] = newAdressBookmark
+	return bookmarks
+}
+
+func removeBookmark(bookmarks map[string]string) map[string]string {
+	var bookmarkToDelete string
+	fmt.Println("Please input bookmark name")
+	fmt.Scan(&bookmarkToDelete)
+	delete(bookmarks, bookmarkToDelete)
+	fmt.Println(bookmarkToDelete, "has been removed")
+	return bookmarks
+
 }
